3378347e6c4f7613699f1c0c4c02b1bc240c4766
Oliver Hahn <olliehahn1357@gmail.com>
2020-03-15 15:14:09 +0100

    Update with implemented closed_form function

--- Project2_Digit-Recognition-Part-1/linear_regression.py
+++ Project2_Digit-Recognition-Part-1/linear_regression.py
@@ -15,8 +15,8 @@ def closed_form(X, Y, lambda_factor):
         theta - (d + 1, ) NumPy array containing the weights of linear regression. Note that theta[0]
         represents the y-axis intercept of the model and therefore X[0] = 1
     """
-    # YOUR CODE HERE
-    raise NotImplementedError
+    #Closed Form Solution of Linear Regression
+    return np.linalg.inv(X.T @ X + lambda_factor * np.identity(X.shape[1])) @ X.T @ Y
 
 ### Functions which are already complete, for you to use ###
 
