43d32e92c3aacd8b46210eea7133b5819603668c
Oliver Hahn <olliehahn1357@gmail.com>
2020-04-07 21:27:42 +0200

    Update ReLU function and its derivate function

--- Project3_Digit-Recognition-Part-2/neural_nets.py
+++ Project3_Digit-Recognition-Part-2/neural_nets.py
@@ -15,11 +15,14 @@ import math
 
 def rectified_linear_unit(x):
     """ Returns the ReLU of x, or the maximum between 0 and x."""
-    # TODO
+    return max(0, x)
 
 def rectified_linear_unit_derivative(x):
     """ Returns the derivative of ReLU."""
-    # TODO
+    if x <= 0:
+        return 0
+    else:
+        return 1
 
 def output_layer_activation(x):
     """ Linear function, returns input as is. """
