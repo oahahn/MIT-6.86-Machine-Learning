35e931ab86d33f14333797c481405e86e7457e33
Oliver Hahn <olliehahn1357@gmail.com>
2020-05-12 20:49:18 +0200

    Compare clustering obtained via K-means vs EM

--- Project4_Collaborative-Filtering/main.py
+++ Project4_Collaborative-Filtering/main.py
@@ -1,9 +1,26 @@
 import numpy as np
-#import kmeans
+import kmeans
 import common
 #import naive_em
 #import em
 
 X = np.loadtxt("toy_data.txt")
 
-# TODO: Your code here
+def run_kmeans():
+    for K in range(1, 5):
+        min_cost = None
+        best_seed = None
+        for seed in range(5):
+            mixture, post = common.init(X, K, seed)
+            mixture, post, cost = kmeans.run(X, mixture, post)
+            if min_cost is None or cost < min_cost:
+                min_cost = cost
+                best_seed = seed
+
+        mixture, post = common.init(X, K, best_seed)
+        mixture, post, cost = kmeans.run(X, mixture, post)
+        title = "K-means for K = {}, seed = {}, cost = {}".format(K, best_seed, min_cost)
+        print(title)
+        common.plot(X, mixture, post, title)
+
+run_kmeans()
