8335585fffee27f9e3dea8c9ec9c35289c4f4e0f
Oliver Hahn <olliehahn1357@gmail.com>
2020-05-13 20:52:42 +0200

    Added estep, mstep and run functions

--- Project4_Collaborative-Filtering/naive_em.py
+++ Project4_Collaborative-Filtering/naive_em.py
@@ -4,7 +4,6 @@ import numpy as np
 from common import GaussianMixture
 
 
-
 def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
     """E-step: Softly assigns each datapoint to a gaussian component
 
@@ -17,7 +16,38 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
             for all components for all examples
         float: log-likelihood of the assignment
     """
-    raise NotImplementedError
+    n = X.shape[0]
+    K = mixture.mu.shape[0]
+    soft_counts = np.zeros((n, K))
+    ll = 0
+
+    for i in range(n):
+        for j in range(K):
+            likelihood = gaussian_likelihood(X[i], mixture.mu[j], mixture.var[j])
+            soft_counts[i, j] = mixture.p[j]*likelihood
+        total_counts = soft_counts[i, :].sum()
+        soft_counts[i, :] = soft_counts[i, :] / total_counts
+        ll += np.log(total_counts)
+
+    return soft_counts, ll
+
+
+def gaussian_likelihood(x, mean, var):
+    """Computes the likelihood of x being generated from a multi-dimensional 
+    Gaussian with uncorrelated components
+    
+    Args: 
+        x: (d, ) the feature vector
+        mean: (d, ) mean of the Gaussian
+        var: scalar, variance of the Gaussian
+
+    Returns:
+        float: the likelihood
+    """
+    d = len(x)
+    result = 1/((2 * np.pi * var)**(d / 2.0))
+    exponent = (-1/(2*var)) * ((x - mean)**2).sum()
+    return result * np.exp(exponent)
 
 
 def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
@@ -32,7 +62,34 @@ def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    raise NotImplementedError
+    n, d = X.shape
+    _, K = post.shape
+    mu_hat = np.zeros((K, d))
+    var_hat = np.zeros(K)
+
+    n_hat = post.sum(axis=0) # 1 x K
+    p_hat = n_hat / n # 1 x K
+
+    for j in range(K):
+        # Computing the mean for each cluster j = 1,...,K
+        # Multiply each x-vector by the soft counts for this cluster j
+        px = X * post[:, j, None] # n x d
+        # Sum these over the n data points
+        px = px.sum(axis=0) # 1 x d
+        # Normalise the values
+        mu_hat[j] = px / n_hat[j] # 1 x d
+
+        # Computing the variance for each cluster j = 1,...,K
+        # Taking the difference of the x-values and the mean squared
+        sqr_diff = (X-mu_hat[j])**2 # n x d
+        # Multiplying each value by the soft counts for this cluster j
+        p_sqr_diff = sqr_diff * post[:, j, None]  # n x d
+        # Summing over all the n's and d's
+        p_sqr_diff = p_sqr_diff.sum() # scalar
+        # Normalising the values
+        var_hat[j] = p_sqr_diff / (d * n_hat[j]) # scalar
+
+    return GaussianMixture(mu_hat, var_hat, p_hat)
 
 
 def run(X: np.ndarray, mixture: GaussianMixture,
@@ -50,4 +107,11 @@ def run(X: np.ndarray, mixture: GaussianMixture,
             for all components for all examples
         float: log-likelihood of the current assignment
     """
-    raise NotImplementedError
+    last_ll = None
+    ll = None
+    while (last_ll is None or ll - last_ll > 1e-6 * np.abs(ll)):
+        last_ll = ll
+        soft_counts, ll = estep(X, mixture)
+        mixture = mstep(X, soft_counts)
+
+    return mixture, soft_counts, ll
